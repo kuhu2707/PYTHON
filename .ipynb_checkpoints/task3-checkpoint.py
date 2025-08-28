de51efff1bf301c47a7c0cff5b3df29759001bae
kuhu2707 <kuhukhandelwal2273@gmail.com>
2025-08-28 13:57:37 +0530

    add task 3 script

---
+++ .ipynb_checkpoints/task3-checkpoint.py
@@ -0,0 +1,6 @@
+#TASK 3 - LOAD A CSV FILE WITH PANDAS , CLEAN IT(HANDLE MISSING VALUES ) AND EXPORT A SUMMARY
+
+import pandas as pd
+
+housing = pd.read_csv(r"C:\Users\ASUS\Downloads\archive (5)\housing.csv")
+
